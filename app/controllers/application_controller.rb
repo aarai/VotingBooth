4e3c13aba7294d7e89c5001f19edbf1d568bbea3
Rai <zclinux@gmail.com>
2012-06-27 15:49:14 -0400

    First commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,5 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery
+
+ # before_filter :authenticate_admin!, :except => [:index,:show]
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectFinal::Application.config.secret_token = '0ad2bda3079b0e4b7915ffc03c8f14c171be4d5df1a7f8995d2f3205d2cb11dc44c342b1c3c3b704ab10f44804e0e63b5054ef03984a9f78e321909dd4ca8354'
